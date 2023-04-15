7ff11287af8121e1d3ad34262ae36c1f9e95f027
mossivy <hmossjvy@gmail.com>
2023-04-15 17:30:15 -0400

    added nav bar

--- prisoners-dilemna/src/App.js
+++ prisoners-dilemna/src/App.js
@@ -4,6 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <nav>
+        <i><h2>The Prisoners Dilemna</h2></i>
+        <ul class="nav-links">
+          <li class="nav-item"><a href="#">Login</a></li>                             
+          <li class="nav-item"><a href="#">Create Account</a></li>    
+        </ul>
+      </nav>
       <header className="App-header">
         <img src={background} className="Background" alt="prison" />
       </header>
