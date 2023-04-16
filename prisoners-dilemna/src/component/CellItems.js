e1b618e28df75276b4114d34b27da8209cc07f7b
Ayden Guinn <aydensspam@gmail.com>
2023-04-16 14:11:48 -0400

    Added Delete by name and index

--- prisoners-dilemna/src/component/CellItems.js
+++ prisoners-dilemna/src/component/CellItems.js
@@ -6,4 +6,18 @@ const cellItems = [
     new Item('Item 3', 'This is the third item', 'Image Link', 'YYYY-MM-DD')
   ];
   
+  function deleteByIndex(index) {
+    cellItems.splice(index, 1);
+  } // deleteByIndex
+
+  function deleteByName(name) {
+    for (let i = 0; i < cellItems.length; i++){
+        if (cellItems[i].title == name) {
+        cellItems.splice(i, 1);
+        break;
+        }
+    } 
+  } // deleteByName
+ 
+
   export default cellItems;
