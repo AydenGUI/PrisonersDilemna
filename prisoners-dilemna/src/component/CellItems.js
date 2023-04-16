a24182a53c1cfbef687e84416e5b187837021953
Ayden Guinn <aydensspam@gmail.com>
2023-04-16 14:01:35 -0400

    Separated CellItem

---
+++ prisoners-dilemna/src/component/CellItems.js
@@ -0,0 +1,9 @@
+import Item from 'Items';
+
+const cellItems = [
+    new Item('Item 1', 'This is the first item', 'Image Link', 'YYYY-MM-DD'),
+    new Item('Item 2', 'This is the second item', 'Image Link', 'YYYY-MM-DD'),
+    new Item('Item 3', 'This is the third item', 'Image Link', 'YYYY-MM-DD')
+  ];
+  
+  export default cellItems;
