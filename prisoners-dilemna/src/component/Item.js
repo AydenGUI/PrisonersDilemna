02976e81f21499a6cdabd131d00be9493ed71059
Ayden Guinn <aydensspam@gmail.com>
2023-04-16 13:47:31 -0400

    Added Item class w/ getters and setters

--- prisoners-dilemna/src/component/Item.js
+++ prisoners-dilemna/src/component/Item.js
@@ -25,6 +25,47 @@ const cellItems = [
   
   export default items;
 
+  class Item {
+    constructor(title, description, imageLink, publishedDate) {
+      this._title = title;
+      this._description = description;
+      this._imageLink = imageLink;
+      this._publishedDate = publishedDate;
+    }
+  
+    get title() {
+      return this._title;
+    }
+  
+    set title(title) {
+      this._title = title;
+    }
+  
+    get description() {
+      return this._description;
+    }
+  
+    set description(description) {
+      this._description = description;
+    }
+  
+    get imageLink() {
+      return this._imageLink;
+    }
+  
+    set imageLink(imageLink) {
+      this._imageLink = imageLink;
+    }
+  
+    get publishedDate() {
+      return this._publishedDate;
+    }
+  
+    set publishedDate(publishedDate) {
+      this._publishedDate = publishedDate;
+    }
+  }
+  
 // function Item() {
 //     // return (
 //     //     <div className="Home">
