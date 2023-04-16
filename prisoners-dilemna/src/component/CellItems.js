3bac7fc7948d9d88dbb967ffff779c7d2f5e53c3
Ayden Guinn <aydensspam@gmail.com>
2023-04-16 14:20:17 -0400

    Fixed export

--- prisoners-dilemna/src/component/CellItems.js
+++ prisoners-dilemna/src/component/CellItems.js
@@ -1,9 +1,9 @@
-import Item from 'Items';
+//import Item from 'Items';
 
 const cellItems = [
-    new Item('Item 1', 'This is the first item', 'Image Link', 'YYYY-MM-DD'),
-    new Item('Item 2', 'This is the second item', 'Image Link', 'YYYY-MM-DD'),
-    new Item('Item 3', 'This is the third item', 'Image Link', 'YYYY-MM-DD')
+    // new Item('Item 1', 'This is the first item', 'Image Link', 'YYYY-MM-DD'),
+    // new Item('Item 2', 'This is the second item', 'Image Link', 'YYYY-MM-DD'),
+    // new Item('Item 3', 'This is the third item', 'Image Link', 'YYYY-MM-DD')
   ];
   
   function deleteByIndex(index) {
@@ -20,4 +20,4 @@ const cellItems = [
   } // deleteByName
  
 
-  export default cellItems;
+  export { cellItems, deleteByIndex, deleteByName };
--- prisoners-dilemna/src/component/Item.js
+++ prisoners-dilemna/src/component/Item.js
@@ -2,29 +2,6 @@ import background from '../assests/1203prison.jpg';
 import './LoggedIn.css';
 import { Link } from 'react-router-dom';
 
-const cellItems = [
-    {
-      title: 'Item 1',
-      description: 'This is the first item',
-      imageLink: 'Image Link',
-      publishedDate: 'YYYY-MM-DD',
-    },
-    {
-      title: 'Item 2',
-      description: 'This is the second item',
-      imageLink: 'Image Link',
-      publishedDate: 'YYYY-MM-DD',
-    },
-    {
-      title: 'Item 3',
-      description: 'This is the third item',
-      imageLink: 'Image Link',
-      publishedDate: 'YYYY-MM-DD',
-    },
-  ];
-  
-  export default cellItems;
-
   class Item {
     constructor(title, description, imageLink, publishedDate) {
       this._title = title;
@@ -66,18 +43,4 @@ const cellItems = [
     }
   }
   
-// function Item() {
-//     // return (
-//     //     <div className="Home">
-//     //         <nav>
-//     //              <i><h2>The Prisoners Dilemna</h2></i>
-//     //              <ul class="nav-links">
-//     //             <li class="nav-item"><Link to="/">Log Out</Link></li>   
-//     //             </ul>
-//     //         </nav>
-//     //     <div class="banner"></div>
-//     // </div>
-//     // );
-// }
-
-// export default Item;
+ export default Item;
