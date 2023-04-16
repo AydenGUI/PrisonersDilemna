8be13bd1293e6668eeb22290c70beb33d6dc64b4
Ayden Guinn <aydensspam@gmail.com>
2023-04-16 13:41:37 -0400

    added item array

--- prisoners-dilemna/src/component/Item.js
+++ prisoners-dilemna/src/component/Item.js
@@ -2,18 +2,41 @@ import background from '../assests/1203prison.jpg';
 import './LoggedIn.css';
 import { Link } from 'react-router-dom';
 
-function Item() {
-    return (
-        <div className="Home">
-            <nav>
-                 <i><h2>The Prisoners Dilemna</h2></i>
-                 <ul class="nav-links">
-                <li class="nav-item"><Link to="/">Log Out</Link></li>   
-                </ul>
-            </nav>
-        <div class="banner"></div>
-    </div>
-    );
-}
+const cellItems = [
+    {
+      title: 'Item 1',
+      description: 'This is the first item',
+      imageLink: 'Image Link',
+      publishedDate: 'YYYY-MM-DD',
+    },
+    {
+      title: 'Item 2',
+      description: 'This is the second item',
+      imageLink: 'Image Link',
+      publishedDate: 'YYYY-MM-DD',
+    },
+    {
+      title: 'Item 3',
+      description: 'This is the third item',
+      imageLink: 'Image Link',
+      publishedDate: 'YYYY-MM-DD',
+    },
+  ];
+  
+  export default items;
 
-export default Item;
+// function Item() {
+//     // return (
+//     //     <div className="Home">
+//     //         <nav>
+//     //              <i><h2>The Prisoners Dilemna</h2></i>
+//     //              <ul class="nav-links">
+//     //             <li class="nav-item"><Link to="/">Log Out</Link></li>   
+//     //             </ul>
+//     //         </nav>
+//     //     <div class="banner"></div>
+//     // </div>
+//     // );
+// }
+
+// export default Item;
