722e45eb7a20a49e38635e76860c2ec5085fc220
mossivy <hmossjvy@gmail.com>
2023-04-22 12:57:04 -0400

    basic login page

--- prisoners-dilemna/src/component/LoggedIn.js
+++ prisoners-dilemna/src/component/LoggedIn.js
@@ -1,8 +1,27 @@
 import background from '../assests/1203prison.jpg';
 import { Link } from 'react-router-dom';
 import './LoggedIn.css';
+import React, { useState } from 'react';
 
 function LoggedIn() { 
+   
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Handle form submission
+  };
+
+    
     return (
         <div className="Home">
         <nav>
@@ -13,12 +32,23 @@ function LoggedIn() {
         </nav>
 
         <div class="banner">
-            <ul class="nav-links">
+            <form onSubmit={handleSubmit}>
+        <label>
+         Email:
+         <input type="email" value={email} onChange={handleEmailChange} />
+        </label>
+      < label>
+         Password:
+            <input type="password" value={password} onChange={handlePasswordChange} />
+         </label>
+        <button type="submit">Submit</button>
+        </form>
+            {/* <ul class="nav-links">
                 <li class="nav-item"><Link to="/Items">Items</Link></li>
                 <li class="nav-item"><Link to="/CreateItems">Create Items</Link></li>
                 <li class="nav-item"><Link to="/UpdateItems">Update Items</Link></li>
                 <li class="nav-item"><Link to="/DeleteItems">Delete Items</Link></li>
-            </ul>
+            </ul> */}
 
             {/* // stuff goes on top of the banner here */}
         </div>
