c574cb85dc9c845193358453a6e85219186bceba
Ayden Guinn <aydensspam@gmail.com>
2023-04-22 15:09:05 -0400

    Added Item Schema

--- prisoners-dilemna/client/src/component/Item.js
+++ prisoners-dilemna/client/src/component/Item.js
@@ -1,46 +1,24 @@
-import background from '../assests/1203prison.jpg';
+// import background from '../assests/1203prison.jpg';
 import './LoggedIn.css';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
-  class Item {
-    constructor(title, description, imageLink, publishedDate) {
-      this._title = title;
-      this._description = description;
-      this._imageLink = imageLink;
-      this._publishedDate = publishedDate;
-    }
-  
-    get title() {
-      return this._title;
-    }
-  
-    set title(title) {
-      this._title = title;
-    }
-  
-    get description() {
-      return this._description;
-    }
-  
-    set description(description) {
-      this._description = description;
-    }
-  
-    get imageLink() {
-      return this._imageLink;
-    }
-  
-    set imageLink(imageLink) {
-      this._imageLink = imageLink;
-    }
-  
-    get publishedDate() {
-      return this._publishedDate;
-    }
-  
-    set publishedDate(publishedDate) {
-      this._publishedDate = publishedDate;
-    }
-  }
-  
- export default Item;
+  const mongoose = require('mongoose');
+
+  const ItemSchema = new mongoose.Schema({
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+    updated_Date: {
+      type: Date,
+      default: Date.now,
+    },
+  });
+  module.exports = Item = mongoose.model('item', ItemSchema);
+//  export default Item;
