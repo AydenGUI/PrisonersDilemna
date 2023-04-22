8cbfd2cad0258715bc1b6554660a0ea94c878ece
mossivy <hmossjvy@gmail.com>
2023-04-22 14:32:24 -0400

    server setup

---
+++ prisoners-dilemna/client/src/App.js
@@ -0,0 +1,29 @@
+import background from './assests/1203prison.jpg';
+import './App.css';
+import Home from './component/Home'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import LoggedIn from './component/LoggedIn';
+import Item from './component/Item';
+import UpdateItemInfo from './component/UpdateItemInfo';
+import CreateItem from './component/CreateItem';
+import DeleteItem from './component/DeleteItem';
+
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+        <Route exact path='/' element={<Home/>}/>
+        <Route exact path='/LoggedIn' element={<LoggedIn/>}/>          
+        <Route exact path='/Items' element={<Item/>}/>
+        <Route exact path='/CreateItems'element={<CreateItem/>}/>
+        <Route exact path='/UpdateItems' element={<UpdateItemInfo/>}/>
+        <Route exact path='/DeleteItems' element={<DeleteItem/>}/>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ prisoners-dilemna/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const server = express();
+server.get('/', (req, res) => res.send('Hello world!'));
+const port = process.env.PORT || 4000;
+
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+// Connect Database
+server.use(cors({ origin: true, credentials: true}));
+
+server.use(express.json({extended: false}));
+
+server.get('/', (req, res) => res.send('Hello world!'));
+
+const conn_str = 'mongodb+srv://saw31815:prisoners-dilemna@prisonerdilemma.ay7xoha.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.set('strictQuery', false);
+mongoose.connect(conn_str,{
+  useUnifiedTopology : true,
+  useNewUrlParser : true
+})
+.then(() => {
+  server.listen(port)
+  console.log("MongoDB Connection Succeeded...")
+})
+.catch(err => {
+  console.log(`Error in DB Connection ${err}`)
+});
