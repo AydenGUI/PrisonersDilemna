ef79839499194a193d742e81daf29b783fcf5f91
Sam Wilson <saw31815@uga.edu>
2023-04-25 14:37:28 -0400

    dilemna

--- prisoners-dilemna/client/src/component/ListItems.js
+++ prisoners-dilemna/client/src/component/ListItems.js
@@ -44,7 +44,7 @@ function ListItems () {
     return (
         <div className="ListItems">
             <nav>
-                <i><h2>The Prisoners Dilemna</h2></i>
+                <i><h2>The Prisoners Dilemma</h2></i>
                 <ul class="nav-links">
                 <li class="nav-item"><Link to="/">Log Out</Link></li>   
                 </ul>
